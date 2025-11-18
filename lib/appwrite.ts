563367a2117f988b87fa13ee03ae243accd1eb9c
Sayan Paul <sayanpaul666.ap@gmail.com>
2025-11-19 01:45:27 +0530

    added tabs

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -8,3 +8,9 @@ export const client = new Client()
 
 // Initialize the Appwrite Account service
 export const account = new Account(client)
+
+// Initialize the Appwrite Databases service
+export const databases = new Databases(client)
+
+export const DATABASE_ID = process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID!
+export const HABITS_COLLECTION_ID = process.env.EXPO_PUBLIC_APPWRITE_HABITS_COLLECTION_ID!
